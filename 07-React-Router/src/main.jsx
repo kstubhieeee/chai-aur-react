6ea48b4527160acce597029bac6e100d3bd97bf5
kstubhie <banekaustubh27@gmail.com>
2024-09-28 00:50:59 +0530

    basic routing commit

--- 07-React-Router/src/main.jsx
+++ 07-React-Router/src/main.jsx
@@ -1,6 +1,47 @@
-
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import { Route } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(<App />,)
+//createBrowserRouter() ye method mei ek array hota hai
+/*
+ const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "/About",
+                element: <About />
+            },
+            {
+                path: "/Contact",
+                element: <Contact />
+            }
+        ]
+    }
+])
+*/
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout />} >
+            <Route path='' element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+        </Route>
+    )
+)
+
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
