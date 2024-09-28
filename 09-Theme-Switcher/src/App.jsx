9622d3d63dae4f9fc4da702016524d1a4a314c48
kstubhie <banekaustubh27@gmail.com>
2024-09-28 16:39:35 +0530

    styling

--- 09-Theme-Switcher/src/App.jsx
+++ 09-Theme-Switcher/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex bg-yellow-500 flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
