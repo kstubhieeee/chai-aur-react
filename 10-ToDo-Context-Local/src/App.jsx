ae1a75e5110f7f197a9c0eb54e4b28d6c392c115
kstubhie <banekaustubh27@gmail.com>
2024-10-01 02:03:39 +0530

    localstorage with context

--- 10-ToDo-Context-Local/src/App.jsx
+++ 10-ToDo-Context-Local/src/App.jsx
@@ -1,13 +1,74 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
+import { useState, useEffect } from 'react'
 import './App.css'
+import { TodoProvider } from './context'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prevTodo) => [...prevTodo, { id: Date.now(), ...todo }])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prevArr) => prevArr.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+    // prev.map((eachValue) => {
+    //   if (eachValue.id === id) {
+    //     return todo
+    //   }
+    //   else eachValue
+    // })
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prevArr) => prevArr.filter((prevTodo) => prevTodo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prevArr) => prevArr.map((prevTodo) => (prevTodo.id === id ?
+      { ...prevTodo, completed: !prevTodo.completed } : prevTodo)))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
 
   return (
-    <>
-      <h1 className='bg-slate-500'>hello</h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {
+              todos.map((todo) => (
+                <div key={todo.id} className='w-full'>
+                  <TodoItem todo={todo} />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   )
 }
 
---
+++ 10-ToDo-Context-Local/src/components/TodoForm.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { useToDo } from "../context/ToDoContext"
+import { useState } from 'react';
+function TodoForm() {
+
+    const [todo, setTodo] = useState("")
+    const { addTodo } = useToDo()
+
+    const add = (event) => {
+        event.preventDefault()
+
+        if (!todo) return
+
+        addTodo({ todo: todo, completed: false })
+        setTodo("")
+    }
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                value={todo}
+                onChange={(event) => setTodo(event.target.value)}
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
